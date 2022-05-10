0d456e4c7a4922aa33993d070a66a6efe7fd775f
Daniel Thomas <geekdan@gmail.com>
2022-05-10 20:25:34 +1000

    Facepalm

--- setup/install-and-configure-all.sh
+++ setup/install-and-configure-all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+
 if uname -a | grep -q "Darwin"; then
 	# echo "we're on a Mac!"
-    source $0/mac/install-mac-tools.sh
+    source $BASEDIR/mac/install-mac-tools.sh
 fi
 
 if uname -a | grep -q "Linux"; then
 	# echo "we're on a Linux!"
-   source $0/linux/install-tools.sh
+   source $BASEDIR/linux/install-tools.sh
 
 fi
 
